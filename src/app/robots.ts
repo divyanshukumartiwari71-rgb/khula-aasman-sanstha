f3013c67a8dc94d686834bfac27215e49261d4d2
divyanshukumartiwari71-rgb <divyanshukumartiwari71@gmail.com>
2026-09-11 09:11:31 +0530

    Fix sitemap domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://khulaaasmansanstha.org';
+  const baseUrl = 'https://khulaaasmansanstha.com';
 
   return {
     rules: {
